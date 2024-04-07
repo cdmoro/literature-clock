7de1196014387e9e06aa1fc3d48f3c0b95b596f3
Carlos Bonadeo <carlosbonadeo@gmail.com>
2024-04-07 09:59:04 +0000

    Style improvements

--- js/themes.js
+++ js/themes.js
@@ -1,10 +1,18 @@
 function getTheme(newTheme) {
-    let theme = "base-dark";
+    let defaultTheme = "base-dark";
     const themeLocalStorage = localStorage.getItem("theme");
     const urlParams = new URLSearchParams(window.location.search);
     const themeQueryParam = urlParams.get('theme');
+    const variantSelect = document.getElementById('variant-select');
+    const variant = variantSelect.value;
 
-    return themeQueryParam || newTheme || themeLocalStorage || theme;
+    let theme = themeQueryParam || newTheme || themeLocalStorage || defaultTheme;
+
+    if (theme.indexOf('-') < 0) {
+        theme += `-${variant}`;
+    }
+
+    return theme;
 }
 
 export function setTheme(newTheme) {
@@ -20,7 +28,6 @@ export function setTheme(newTheme) {
     }
 
     themeSelect.value = theme.split('-')[0];
-    console.log(theme, theme.indexOf('-') >= 0 ? theme.split('-')[1] : 'system');
     variantSelect.value = theme.indexOf('-') >= 0 ? theme.split('-')[1] : 'system';
     localStorage.setItem('theme', theme);
     document.documentElement.setAttribute("data-theme", theme);
