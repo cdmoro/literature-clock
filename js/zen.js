f1760aba86ff1a736dc0923286c05f3959e51e06
Carlos Bonadeo <carlosbonadeo@gmail.com>
2024-04-17 23:41:39 +0100

    Fix zen mode

--- js/zen.js
+++ js/zen.js
@@ -1,19 +1,19 @@
-import { getBooleanSetting, setBooleanSetting } from "./settings.js";
+import {
+  deleteUrlParamAndRefresh,
+  getBooleanSetting,
+  setBooleanSetting,
+} from "./settings.js";
 
 export function initZenMode(defaultValue = false) {
   const value = getBooleanSetting("zen", defaultValue);
   setBooleanSetting("zen", value);
 
-  document
-    .getElementById("zen")
-    .addEventListener("click", () => {
-      setBooleanSetting("zen", true);
-      deleteUrlParamAndRefresh("zen");
-    });
-  document
-    .getElementById("exit-zen")
-    .addEventListener("click", () => {
-      setBooleanSetting("zen", false);
-      deleteUrlParamAndRefresh("zen");
-    });
+  document.getElementById("zen").addEventListener("click", () => {
+    setBooleanSetting("zen", true);
+    deleteUrlParamAndRefresh("zen");
+  });
+  document.getElementById("exit-zen").addEventListener("click", () => {
+    setBooleanSetting("zen", false);
+    deleteUrlParamAndRefresh("zen");
+  });
 }
