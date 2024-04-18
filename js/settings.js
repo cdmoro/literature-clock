6e3327b05e6e99c9ca4578b9d5ee1fdd85b7b756
Carlos Bonadeo <carlosbonadeo@gmail.com>
2024-04-18 23:09:38 +0100

    Remove unused ode

--- js/settings.js
+++ js/settings.js
@@ -56,19 +56,9 @@ export function setStringSetting(name, value) {
   localStorage.setItem(name, value);
 }
 
-// deprecated
-export function deleteUrlParamIfExistsAndRefresh(name) {
-  /*const urlParams = new URLSearchParams(window.location.search);
-
-  if (urlParams.has(name)) {
-    urlParams.delete(name);
-    window.location.search = urlParams.toString();
-  }*/
-}
-
 export function updateURL(name, value) {
   const urlParams = new URLSearchParams(window.location.search);
   urlParams.set(name, value);
 
-  history.replaceState({}, "", `?${urlParams.toString()}`)
+  history.replaceState({}, "", `?${urlParams.toString()}`);
 }
